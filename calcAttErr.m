b1415eefa1b2c9313193c1d18a49ead4b890e571
Brian Amin <bhautikamin97@gmail.com>
2022-04-04 10:34:21 -0400

    Figured out error - fixed mrp to dcm conversion

--- calcAttErr.m
+++ calcAttErr.m
@@ -1,11 +1,10 @@
 function [o_b_r, b_w_b_r] = calcAttErr(o_b_n, b_w_b_n, RN, n_w_r_n)
 % Calculate Attitude and Angular velocity tracking error of current body
 % frame B relative to reference frame R
-% o_b_r = o_b_n - o_r_n
-%o_b_r = o_b_n - dcm2mrp(RN);
 
 BN = mrp2dcm(o_b_n);
-o_b_r = dcm2mrp(BN*RN');
+NR = RN';
+o_b_r = dcm2mrp(BN*NR);
 b_w_b_r = (b_w_b_n - BN * n_w_r_n);
 
 
